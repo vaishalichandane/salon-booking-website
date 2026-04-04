788e419b0c5e4c91b28658060aa9ec31135278eb
vaishalichandane <vaishalichandane543@gmail.com>
2026-04-04 11:09:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// DARK MODE TOGGLE
+let modeBtn = document.getElementById("modeBtn");
+
+if (modeBtn) {
+    modeBtn.addEventListener("click", function() {
+
+        document.body.classList.toggle("dark-mode");
+
+        // Change button text
+        if (document.body.classList.contains("dark-mode")) {
+            modeBtn.innerText = "☀ Light Mode";
+        } else {
+            modeBtn.innerText = "🌙 Dark Mode";
+        }
+
+    });
+}
+
+// BOOKING FORM HANDLING
+let form = document.getElementById("bookingForm");
+let result = document.getElementById("result");
+
+if (form) {
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        result.innerText = "Thank you! We will contact you soon.";
+        form.reset();
+    });
+}
+
+console.log("JavaScript Connected Successfully!");
